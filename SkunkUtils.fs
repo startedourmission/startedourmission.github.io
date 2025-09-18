550ab145d8f27f428bbf29c117a6a487b122915e
startedourmission <hanuiwind4@gmail.com>
2025-09-18 10:56:06 +0900

    new write: 2025-09-18 10:56:06

--- SkunkUtils.fs
+++ SkunkUtils.fs
@@ -132,7 +132,7 @@ module Obsidian =
                 let imageUrl = $"images/{imageName}"
                 // URL 인코딩 처리 (공백 등)
                 let encodedUrl = System.Uri.EscapeUriString(imageUrl)
-                $"<img src=\"{encodedUrl}\" alt=\"{imageName}\" class=\"embedded-image\">")
+                $"<img src=\"{encodedUrl}\" alt=\"{imageName}\" class=\"embedded-image\">"
             else
                 // 페이지 링크 처리 [[Page Name]] or [[Page Name|Display Text]]
                 let linkText = linkContent
